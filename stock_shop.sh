c2f3b49134c2e516a482e84c5d607bd4956d70ad
paperspace <byl006@ucsd.edu>
2023-06-16 00:09:27 +0000

    fixed paths in stock_shop.sh

--- stock_shop.sh
+++ stock_shop.sh
@@ -30,7 +30,7 @@ hpa="$gi/hcr_probes_all_hairpins"
 bio fetch $gn > $pr/$gn.ncbi_report.txt
 
 # obtains annotation for this probe target
-bio fetch $gn --format gff > $gi/lp.v2.$pr.annotation_report.gff3
+bio fetch $gn --format gff > $pr/$gn.annotation_report.gff3 
 
 # obtains CDS sequence of probe target
 bio fetch $gn | bio fasta --type CDS > $pr/$gn.cds_report.txt
@@ -38,21 +38,18 @@ bio fetch $gn | bio fasta --type CDS > $pr/$gn.cds_report.txt
 # remove headers and whitespace from cds_report
 cat $pr/$gn.cds_report.txt | tail -n +2 | tr -d " \t\n\r" > $prc/$gn.cleaned.txt
 
-#cp ~/yl_pictus-2/lp_transcriptome/csv_to_xlsx.py ~/yl_pictus-2/lp_transcriptome/sp_transcripts_of_interest_test/lp.v2.$gene_name/lp.v2.$gene_name.hcr_probes_all_hairpins/csv_to_xlsx.py
+mkdir -p "$hpa/csv"
 
 for i in "b1" "b2" "b3" "b4" "b5"
 do
-   python "$ps"/ipg/make_probe.py "sp" $gn $i $prc/$gn.cleaned.txt > $hpr/$i.$gene_name.probegen_standard_report.txt
-   begin=$(cat $hpr/$i.$gene_name.probegen_standard_report.txt | grep -Fn "Pool name, Sequence" | cut -d ":" -f 1)
-   end_num=$(cat $hpr/$i.$gene_name.probegen_standard_report.txt | grep -Fn "Figure Layout of Probe Sequences" | cut -d ":" -f 1)
+   python "$ps"/ipg/make_probe.py "sp" $gn $i $prc/$gn.cleaned.txt > $hpr/$i.$gn.probegen_report.txt
+   begin=$(cat $hpr/$i.$gn.probegen_report.txt | grep -Fn "Pool name, Sequence" | cut -d ":" -f 1)
+   end_num=$(cat $hpr/$i.$gn.probegen_report.txt | grep -Fn "Figure Layout of Probe Sequences" | cut -d ":" -f 1)
    end=$(expr "$end_num" - 3)
-   cat $hpr/lp.v2.$gene_name.$i.probegen_standard_report.txt | sed -n "$begin","$end"p | sed 's/ *, */,/' > $hpa/lp.v2.$gene_name.$i.opools_order.csv
-   python ~/yl_pictus-2/lp_transcriptome/sp_transcripts_of_interest_test/lp.v2.$gene_name/lp.v2.$gene_name.hcr_probes_all_hairpins/csv_to_xlsx.py ~/yl_pictus-2/lp_transcriptome/sp_transcripts_of_interest_test/lp.v2.$gene_name/lp.v2.$gene_name.hcr_probes_all_hairpins/lp.v2.$gene_name.$i.opools_order.csv "$gene_name" "$i"
-#done
+   cat $hpr/$i.$gn.probegen_report.txt | sed -n "$begin","$end"p | sed 's/ *, */,/' > $hpa/csv/$i.$gn.opools_order.csv
+   python "$ps"/csv_to_xlsx.py $hpa/csv/$i.$gn.opools_order.csv "$gn" "$i" "$hpa/"
+done
 
-#mv ~/yl_pictus-2/lp_transcriptome/sp_transcripts_of_interest_test/lp.v2.$gene_name/lp.v2.$gene_name.hcr_probes_all_hairpins/lp*order.csv ~/yl_pictus-2/lp_transcriptome/sp_transcripts_of_interest_test/lp.v2.$gene_name/lp.v2.$gene_name.hcr_probes_all_hairpins/csv
-#mv ~/yl_pictus-2/lp_transcriptome/lp.v2.$gene_name*opools_order.xlsx ~/yl_pictus-2/lp_transcriptome/sp_transcripts_of_interest_test/lp.v2.$gene_name/lp.v2.$gene_name.hcr_probes_all_hairpins
-#rm ~/yl_pictus-2/lp_transcriptome/sp_transcripts_of_interest_test/lp.v2.$gene_name/lp.v2.$gene_name.hcr_probes_all_hairpins/csv_to_xlsx.py
-#echo "***** Probes generated for $gene_name *****"
+echo "***** Probes generated for $gene_name *****"
 
 
